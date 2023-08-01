4da7a628bb69843013f80a022ef508ee0b38e900
RAJVARDHAN-M <84311650+RAJVARDHAN-M@users.noreply.github.com>
2023-08-01 23:11:14 +0530

    Done

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -23,7 +23,8 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("/auth/login", credentials);
+      const res = await axios.post("https://maha-booking-app-api.onrender.com/api/auth/login", credentials);
+      console.log(res.data)
       if (res.data.isAdmin) {
         dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
 
